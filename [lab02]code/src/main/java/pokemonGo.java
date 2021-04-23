c2561686cd45565cd30cfcb67d368be4090ce772
Josedlz <jose.delama@utec.edu.pe>
2021-04-23 08:24:31 -0400

    miun no puede pushear

--- [lab02]code/src/main/java/pokemonGo.java
+++ [lab02]code/src/main/java/pokemonGo.java
@@ -6,6 +6,7 @@ public class pokemonGo {
 
     public static void main(String[] args){
         logger.info("CS-UTECd Software Engineering I");
+        logger.info("jaja miun no puede pushear");
     }
 
 
